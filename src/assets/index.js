99f03083b93375ab14ae3dd6bd53b4315da7a67b
DanielNevo <74509153+DanielNevo@users.noreply.github.com>
2023-10-15 15:29:47 +0300

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -24,7 +24,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-import FeedMe from  "./Feed_me.jpeg"
+import FeedMe from  "./Feed_me.png"
 import chat from "./chat.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
